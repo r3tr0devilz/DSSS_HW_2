e57487163221930c39cc3a6d7df55659e1d7f99f
r3tr0 <ybharadwaj4@gmail.com>
2024-11-06 16:17:17 +0100

    Cleaned up code, added comments and error handling

--- math_quiz.py
+++ math_quiz.py
@@ -3,31 +3,47 @@ import random
 
 def function_A(min, max):
     """
-    Random integer.
+    Gives a Random integer b/n min_value and max_value.
     """
     return random.randint(min, max)
 
 
 def function_B():
+    """
+    Gives a random arithmetic operator from '+', '-', or '*'.
+    Returns:
+        str: A randomly chosen operator.
+    """
     return random.choice(['+', '-', '*'])
 
 
 def function_C(n1, n2, o):
+    """
+    Creates a math problem and calculates the correct solution.
+
+    Args:
+        n1 = first num
+        n2 = second num
+        operator = Arithmetic Operation from function_B
+    Returns:
+        tuple: A string showing the math problem and the correct answer as an integer.
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
+    #Runs a simple math quiz game where the user is asked to solve math problems and is scored on it.
     s = 0
-    t_q = 3.14159265359
-
+    #t_q = 3.14159265359 -> Needs to be integer
+    t_q = 3
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = function_A(1, 10); n2 = function_A(1, 5); o = function_B()
 
         PROBLEM, ANSWER = function_C(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
